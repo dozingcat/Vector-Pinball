da3181ac6a961fe518117c7dfe08ef6722a34e12
Brian Nenninger <bnenning@gmail.com>
2011-06-24 23:18:02 -0500

    added background music

--- gen/com/dozingcatsoftware/bouncy/R.java
+++ gen/com/dozingcatsoftware/bouncy/R.java
@@ -14,32 +14,36 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int aboutButton=0x7f060004;
-        public static final int buttonPanel=0x7f060002;
-        public static final int preferencesButton=0x7f060005;
-        public static final int scoreView=0x7f060000;
-        public static final int startGameButton=0x7f060003;
-        public static final int worldView=0x7f060001;
+        public static final int aboutButton=0x7f070004;
+        public static final int buttonPanel=0x7f070002;
+        public static final int preferencesButton=0x7f070005;
+        public static final int scoreView=0x7f070000;
+        public static final int startGameButton=0x7f070003;
+        public static final int worldView=0x7f070001;
     }
     public static final class layout {
         public static final int about=0x7f030000;
         public static final int main=0x7f030001;
     }
+    public static final class raw {
+        public static final int androidpad=0x7f050000;
+        public static final int drumbassloop=0x7f050001;
+    }
     public static final class string {
-        public static final int about_menu_item=0x7f050001;
-        public static final int about_text=0x7f05000d;
-        public static final int app_name=0x7f050000;
-        public static final int end_game_menu_item=0x7f050002;
-        public static final int high_quality_pref_summary_off=0x7f050009;
-        public static final int high_quality_pref_summary_on=0x7f050008;
-        public static final int high_quality_pref_title=0x7f050007;
-        public static final int independent_flippers_pref_summary_off=0x7f050006;
-        public static final int independent_flippers_pref_summary_on=0x7f050005;
-        public static final int independent_flippers_pref_title=0x7f050004;
-        public static final int preferences_menu_item=0x7f050003;
-        public static final int show_fps_pref_title=0x7f05000b;
-        public static final int sound_pref_title=0x7f05000a;
-        public static final int zoom_pref_title=0x7f05000c;
+        public static final int about_menu_item=0x7f060001;
+        public static final int about_text=0x7f06000d;
+        public static final int app_name=0x7f060000;
+        public static final int end_game_menu_item=0x7f060002;
+        public static final int high_quality_pref_summary_off=0x7f060009;
+        public static final int high_quality_pref_summary_on=0x7f060008;
+        public static final int high_quality_pref_title=0x7f060007;
+        public static final int independent_flippers_pref_summary_off=0x7f060006;
+        public static final int independent_flippers_pref_summary_on=0x7f060005;
+        public static final int independent_flippers_pref_title=0x7f060004;
+        public static final int preferences_menu_item=0x7f060003;
+        public static final int show_fps_pref_title=0x7f06000b;
+        public static final int sound_pref_title=0x7f06000a;
+        public static final int zoom_pref_title=0x7f06000c;
     }
     public static final class xml {
         public static final int preferences=0x7f040000;
--- src/com/dozingcatsoftware/bouncy/VPSoundpool.java
+++ src/com/dozingcatsoftware/bouncy/VPSoundpool.java
@@ -1,14 +1,15 @@
 package com.dozingcatsoftware.bouncy;
 
-import android.content.Context;
-import android.content.res.AssetManager;
-import android.media.AudioManager;
-import android.media.SoundPool;
-import android.util.Log;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Random;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
+import android.util.Log;
 
 public class VPSoundpool {
 
@@ -19,106 +20,182 @@ public class VPSoundpool {
     private static Random mRandom = new Random();
     
     private static boolean soundEnabled = true;
+	private static int cScore = 0;
+	private static int prevDing, currentDing = 0;
+	private static int andrModAmt = 10;
+	private static MediaPlayer drumbass;
+	private static boolean drumbassPlaying = false;
+	private static MediaPlayer androidpad;
+    //private final static float SEMITONE = 1.059463094f;
     
-    static final int ID_BUMPER_START = 0;
-    static final int NUM_BUMPER_SOUNDS = 6;
+    static int ID_DING_START 	= 0;
+    static int NUM_DINGS 		= 6;
     
-    static final int ID_LAUNCH = 100;
-    static final int ID_FLIPPER = 101;
-    static final int ID_MESSAGE = 102;
-    static final int ID_START = 103;
+    static int ID_LAUNCH 		= 100;
+    static int ID_FLIPPER 		= 101;
+    static int ID_MESSAGE 		= 102;
+    static int ID_START 		= 103;
     
-    static final int ID_ROLLOVER_START = 200;
-    static final int NUM_ROLLOVER_SOUNDS = 6;
+    static int ID_ROLLOVER 		= 200;
+    
+    static int ID_ANDROIDPAD 	= 300;
+    static int ID_DRUMBASSLOOP 	= 301;
+    
 
     public static void initSounds(Context theContext) 
     { 
          Log.v("VPSoundpool", "initSounds");
          mContext = theContext;
-         mSoundPool = new SoundPool(6, AudioManager.STREAM_MUSIC, 0);
+         mSoundPool = new SoundPool(32, AudioManager.STREAM_MUSIC, 0);
          mSoundPoolMap = new HashMap<Integer, Integer>(); 
          mAudioManager = (AudioManager)mContext.getSystemService(Context.AUDIO_SERVICE);
+         int mMediaVolume = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+         mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, mMediaVolume - 2, 0);
     } 
 
     public static void loadSounds()
     {
         mSoundPoolMap.clear();
-        
         AssetManager assets = mContext.getAssets();
         try {
-            mSoundPoolMap.put(ID_BUMPER_START+0, mSoundPool.load(assets.openFd("audio/bumper/xdinga1.ogg"), 1));
-            mSoundPoolMap.put(ID_BUMPER_START+1, mSoundPool.load(assets.openFd("audio/bumper/xdingc1.ogg"), 1));
-            mSoundPoolMap.put(ID_BUMPER_START+2, mSoundPool.load(assets.openFd("audio/bumper/xdingc2.ogg"), 1));
-            mSoundPoolMap.put(ID_BUMPER_START+3, mSoundPool.load(assets.openFd("audio/bumper/xdingd1.ogg"), 1));
-            mSoundPoolMap.put(ID_BUMPER_START+4, mSoundPool.load(assets.openFd("audio/bumper/xdinge1.ogg"), 1));
-            mSoundPoolMap.put(ID_BUMPER_START+5, mSoundPool.load(assets.openFd("audio/bumper/xdingg1.ogg"), 1));
+            mSoundPoolMap.put(ID_DING_START+0, mSoundPool.load(assets.openFd("audio/bumper/dinga1.ogg"), 1));
+            mSoundPoolMap.put(ID_DING_START+1, mSoundPool.load(assets.openFd("audio/bumper/dingc1.ogg"), 1));
+            mSoundPoolMap.put(ID_DING_START+2, mSoundPool.load(assets.openFd("audio/bumper/dingc2.ogg"), 1));
+            mSoundPoolMap.put(ID_DING_START+3, mSoundPool.load(assets.openFd("audio/bumper/dingd1.ogg"), 1));
+            mSoundPoolMap.put(ID_DING_START+4, mSoundPool.load(assets.openFd("audio/bumper/dinge1.ogg"), 1));
+            mSoundPoolMap.put(ID_DING_START+5, mSoundPool.load(assets.openFd("audio/bumper/dingg1.ogg"), 1));
             
-            mSoundPoolMap.put(ID_ROLLOVER_START+0, mSoundPool.load(assets.openFd("audio/rollover/rolloverC1.ogg"), 1));            
-            mSoundPoolMap.put(ID_ROLLOVER_START+1, mSoundPool.load(assets.openFd("audio/rollover/rolloverD1.ogg"), 1));            
-            mSoundPoolMap.put(ID_ROLLOVER_START+2, mSoundPool.load(assets.openFd("audio/rollover/rolloverE1.ogg"), 1));            
-            mSoundPoolMap.put(ID_ROLLOVER_START+3, mSoundPool.load(assets.openFd("audio/rollover/rolloverG1.ogg"), 1));            
-            mSoundPoolMap.put(ID_ROLLOVER_START+4, mSoundPool.load(assets.openFd("audio/rollover/rolloverA1.ogg"), 1));            
-            mSoundPoolMap.put(ID_ROLLOVER_START+5, mSoundPool.load(assets.openFd("audio/rollover/rolloverC2.ogg"), 1));            
-
             mSoundPoolMap.put(ID_LAUNCH, mSoundPool.load(assets.openFd("audio/misc/andBounce2.ogg"), 1));
             mSoundPoolMap.put(ID_FLIPPER, mSoundPool.load(assets.openFd("audio/misc/flipper1.ogg"), 1));
             mSoundPoolMap.put(ID_MESSAGE, mSoundPool.load(assets.openFd("audio/misc/message2.ogg"), 1));
-            mSoundPoolMap.put(ID_START, mSoundPool.load(assets.openFd("audio/misc/startup1.ogg"), 1));            
-        }
+            mSoundPoolMap.put(ID_START, mSoundPool.load(assets.openFd("audio/misc/startup1.ogg"), 1));
+            mSoundPoolMap.put(ID_ROLLOVER, mSoundPool.load(assets.openFd("audio/misc/rolloverE.ogg"), 1));
+            
+        	drumbass = MediaPlayer.create(mContext, R.raw.drumbassloop);
+        	androidpad = MediaPlayer.create(mContext, R.raw.androidpad);
+
+         }
         catch(IOException ex) {
         	Log.e("VPSoundpool", "Error loading sounds", ex);
         }
+        resetState();
     }
     
     public static void setSoundEnabled(boolean enabled) {
     	soundEnabled = enabled;
     }
     
-    static void playSound(int soundKey, float pitch) {
+    public static void resetState() {
+    	pauseMusic();
+    	cScore = 0;
+    	andrModAmt = 10;
+    }
+    
+    static void playSound(int soundKey, float volume, float pitch) {
     	if (soundEnabled && mSoundPoolMap!=null) {
     		Integer soundID = mSoundPoolMap.get(soundKey);
     		if (soundID!=null) {
-        		mSoundPool.play(soundID, 0.5f, 0.5f, 1, 0, pitch);
+        		mSoundPool.play(soundID, volume, volume, 1, 0, pitch);
     		}
     	}
     }
 
     public static void playScore() {
-    	playSound(ID_BUMPER_START + mRandom.nextInt(NUM_BUMPER_SOUNDS), 1);
+    	//prevent repeated dings
+    	while (currentDing == prevDing) {
+        	currentDing = mRandom.nextInt(NUM_DINGS);    		
+    	}
+    	playSound(ID_DING_START + currentDing, 0.5f, 1);
+    	prevDing = currentDing;
+
+    	//start playing the drumbassloop after 20 scoring hits
+    	cScore++;
+    	if (soundEnabled && cScore%20 == 0 && !drumbass.isPlaying()){
+    		drumbass.setVolume(1.0f, 1.0f);
+    	    drumbass.start();
+    	    drumbassPlaying = true;
+    	}
+    	//play the androidpad after 10 scoring hits the first time
+    	//then increase the mod amount each time the pad is played,
+    	//so that it will be heard less frequently over time
+    	if (soundEnabled && cScore%andrModAmt == 0){
+    		androidpad.setVolume(0.5f, 0.5f);
+    	    androidpad.start();
+    		andrModAmt += 42;
+    	}
     }
 
-    // for rollovers, play chord of 1, 2, or 3 rollover notes
-    static int[] rolloverNotes = new int[3];
-    
     public static void playRollover() {
-    	for(int i=0; i<3; i++) {
-    		rolloverNotes[i] = mRandom.nextInt(NUM_ROLLOVER_SOUNDS);
-    	}
-    	
-    	playSound(ID_ROLLOVER_START + rolloverNotes[0], 1);
-    	if (rolloverNotes[1]!=rolloverNotes[0]) {
-    		playSound(ID_ROLLOVER_START + rolloverNotes[1], 1);
-    	}
-    	if (rolloverNotes[2]!=rolloverNotes[0] && rolloverNotes[2]!=rolloverNotes[1]) {
-    		playSound(ID_ROLLOVER_START + rolloverNotes[2], 1);
-    	}
-    	
+    	//play up to three events, each randomly pitched to a different note in the pentatonic scale
+    	//the rollover ding is E, so in semitones, the other pitches are -4 (C), -2 (D), +3 (G), +5 (A), +8 (C)
+    	//for Soundpool, that translate to:  0.7937008 (C), 0.8908991 (D), 1.1892079 (G), 1.3348408 (A), 1.5874025 (C)
+    	float pitch[] = {0.7937008f, 0.8908991f, 1f, 1.1892079f, 1.3348408f, 1.5874025f};
+    	int pitchDx[] = {0, 0, 0};
+
+        for (int i = 0; i < 3; i++) {
+    		switch (i){
+    			case 0:
+    				pitchDx[i] = mRandom.nextInt(6);
+    		    	playSound(ID_ROLLOVER, .3f, pitch[pitchDx[i]]);
+    				break;
+    			case 1:
+    				pitchDx[i] = mRandom.nextInt(6);
+    				if (pitchDx[i] != pitchDx[i-1]) {
+        		    	playSound(ID_ROLLOVER, .3f, pitch[pitchDx[i]]);
+    				}
+    				break;
+    			case 2:
+    				pitchDx[i] = mRandom.nextInt(6);
+    				if (pitchDx[i] != pitchDx[i-1] &&
+    					pitchDx[i] != pitchDx[i-2] ) {
+        		    	playSound(ID_ROLLOVER, .3f, pitch[pitchDx[i]]);
+    				}
+    				break;
+    			default:
+    				Log.e("VPSoundpool", "rollover bad mojo");
+    				break;
+    		}
+        }
     }
     
     public static void playBall() {
-    	playSound(ID_LAUNCH, 1);
+    	playSound(ID_LAUNCH, 1, 1);
     }
     
     public static void playFlipper() {
-    	playSound(ID_FLIPPER, 1);
+    	playSound(ID_FLIPPER, 1, 1);
     }
     
     public static void playStart() {
-    	playSound(ID_START, 1);
+    	resetState();
+    	playSound(ID_START, 0.5f, 1);
     }
     
     public static void playMessage() {
-    	playSound(ID_MESSAGE, 1);
+    	playSound(ID_MESSAGE, 0.66f, 1);
+    }
+    
+    public static void pauseMusic()
+    {
+    	if (drumbass.isPlaying()) {
+            drumbass.pause();
+    	}
+    	if (androidpad.isPlaying()) {
+    		androidpad.pause();
+    	}
+    }
+    public static void resumeMusic()
+    {
+    	if (drumbassPlaying) {
+    		drumbass.start();
+    	}
+        //androidpad.start();
+    }
+    public static void stopMusic()
+    {
+        drumbass.stop();
+        drumbassPlaying = false;
+        androidpad.stop();
     }
     
     public static void cleanup()
@@ -127,5 +204,9 @@ public class VPSoundpool {
         mSoundPool = null;
         mSoundPoolMap.clear();
         mAudioManager.unloadSoundEffects();
+        drumbass.release();
+        drumbass = null;
+        androidpad.release();
+        androidpad = null;
     }
 }
