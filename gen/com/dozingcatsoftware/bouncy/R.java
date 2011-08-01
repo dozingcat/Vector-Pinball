b507a76507c6ac4b21275c5a54024f31ed11cd6a
Brian Nenninger <bnenning@gmail.com>
2011-08-01 00:51:11 -0500

    added per-table rules in AboutActivity text

--- gen/com/dozingcatsoftware/bouncy/R.java
+++ gen/com/dozingcatsoftware/bouncy/R.java
@@ -14,13 +14,14 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int aboutButton=0x7f070005;
-        public static final int buttonPanel=0x7f070003;
-        public static final int canvasFieldView=0x7f070001;
-        public static final int glFieldView=0x7f070002;
-        public static final int preferencesButton=0x7f070006;
-        public static final int scoreView=0x7f070000;
-        public static final int switchTableButton=0x7f070004;
+        public static final int aboutButton=0x7f070006;
+        public static final int aboutTextView=0x7f070000;
+        public static final int buttonPanel=0x7f070004;
+        public static final int canvasFieldView=0x7f070002;
+        public static final int glFieldView=0x7f070003;
+        public static final int preferencesButton=0x7f070007;
+        public static final int scoreView=0x7f070001;
+        public static final int switchTableButton=0x7f070005;
     }
     public static final class layout {
         public static final int about=0x7f030000;
@@ -45,6 +46,8 @@ public final class R {
         public static final int preferences_menu_item=0x7f060003;
         public static final int show_fps_pref_title=0x7f06000c;
         public static final int sound_pref_title=0x7f06000a;
+        public static final int table1_rules=0x7f060010;
+        public static final int table2_rules=0x7f060011;
         public static final int use_opengl_pref_title=0x7f06000e;
         public static final int zoom_pref_title=0x7f06000d;
     }
