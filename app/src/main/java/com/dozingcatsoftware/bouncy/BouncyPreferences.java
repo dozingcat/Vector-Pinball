c2f0f09213a26b7d70bc640ebc136161bec8b51b
Brian Nenninger <bnenning@gmail.com>
2021-02-22 22:56:54 -0500

    Disable haptic feedback option if device doesn't have a vibrator (#82)

--- app/src/main/java/com/dozingcatsoftware/bouncy/BouncyPreferences.java
+++ app/src/main/java/com/dozingcatsoftware/bouncy/BouncyPreferences.java
@@ -1,12 +1,32 @@
 package com.dozingcatsoftware.bouncy;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.preference.CheckBoxPreference;
 import android.preference.PreferenceActivity;
 
 public class BouncyPreferences extends PreferenceActivity {
+
+    private static boolean supportsMultitouch() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO;
+    }
+
+    private static boolean supportsHapticFeedback(Context context) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.FROYO) {
+            return false;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+            if (vibrator == null || !vibrator.hasVibrator()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
@@ -16,12 +36,12 @@ public class BouncyPreferences extends PreferenceActivity {
         }
 
         // If multitouch or haptic feedback APIs aren't available, disable the preference items.
-        // Coincidentally, both require Froyo (API level 8).
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.FROYO) {
+        if (!supportsMultitouch()) {
             CheckBoxPreference mtPref = (CheckBoxPreference) findPreference("independentFlippers");
             mtPref.setChecked(false);
             mtPref.setEnabled(false);
-
+        }
+        if (!supportsHapticFeedback(this)) {
             CheckBoxPreference hapticPref = (CheckBoxPreference) findPreference("haptic");
             hapticPref.setChecked(false);
             hapticPref.setEnabled(false);
