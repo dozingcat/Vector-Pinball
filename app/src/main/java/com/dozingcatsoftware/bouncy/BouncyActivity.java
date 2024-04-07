f32cd9da5627d7d630fc83a2130d9580a86ebe9b
Kerry Sainsbury <kerry@fidelma.com>
2024-04-07 20:51:15 +1200

    Immediately show use their score at end of game

--- app/src/main/java/com/dozingcatsoftware/bouncy/BouncyActivity.java
+++ app/src/main/java/com/dozingcatsoftware/bouncy/BouncyActivity.java
@@ -1,5 +1,8 @@
 package com.dozingcatsoftware.bouncy;
 
+import static com.dozingcatsoftware.bouncy.ScoreView.LAST_SCORE_MESSAGE;
+import static com.dozingcatsoftware.bouncy.ScoreView.TOUCH_TO_START_MESSAGE;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -494,6 +497,9 @@ public class BouncyActivity extends Activity {
             if (!field.getGameState().isGameInProgress()) {
                 // game just ended, show button panel and set end game timestamp
                 this.endGameTime = System.currentTimeMillis();
+                // always show LAST_SCORE_MESSAGE immediately at the end of a game
+                // (LAST_SCORE_MESSAGE is next after TOUCH_TO_START_MESSAGE)
+                scoreView.gameOverMessageIndex = TOUCH_TO_START_MESSAGE;
                 updateButtons();
 
                 // No high scores for unlimited balls.
