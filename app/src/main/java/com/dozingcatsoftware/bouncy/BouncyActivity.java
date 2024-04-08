4826f4ef18c84cfe2828f45e80e37d075c0535b3
Kerry Sainsbury <kerry@fidelma.com>
2024-04-08 20:13:55 +1200

    Now can see the tables when browsing them (ugly implementation -- sorry!)

--- app/src/main/java/com/dozingcatsoftware/bouncy/BouncyActivity.java
+++ app/src/main/java/com/dozingcatsoftware/bouncy/BouncyActivity.java
@@ -625,6 +625,8 @@ public class BouncyActivity extends Activity {
             // synchronized because that can deadlock the FieldDriver thread.
             // All of this concurrency is badly in need of refactoring.
             synchronized (field) {
+                restoreMenuHeight();
+
                 buttonPanel.setVisibility(View.GONE);
                 highScorePanel.setVisibility(View.GONE);
                 resetFieldForCurrentLevel();
@@ -674,6 +676,8 @@ public class BouncyActivity extends Activity {
     }
 
     void switchToTable(int tableNum) {
+        shrinkMenuToTableSelectionSize();
+
         this.currentLevel = tableNum;
         synchronized (field) {
             resetFieldForCurrentLevel();
@@ -688,6 +692,28 @@ public class BouncyActivity extends Activity {
 
     public void doSwitchTable(View view) {
         doNextTable(view);
+    }
+
+
+    private int menuHeight;
+    private int tableSelectionHeight;
+
+    private void shrinkMenuToTableSelectionSize() {
+        // There must be a better place to obtain this information
+        if (menuHeight == 0) {
+            menuHeight = buttonPanel.getHeight();
+            tableSelectionHeight = previousTableButton.getHeight();
+        }
+
+        buttonPanel.getLayoutParams().height = tableSelectionHeight;
+        buttonPanel.setLayoutParams(buttonPanel.getLayoutParams());
+    }
+
+    private void restoreMenuHeight() {
+        if (menuHeight != 0) {
+            buttonPanel.getLayoutParams().height = menuHeight;
+            buttonPanel.setLayoutParams(buttonPanel.getLayoutParams());
+        }
     }
 
     public void doNextTable(View view) {
